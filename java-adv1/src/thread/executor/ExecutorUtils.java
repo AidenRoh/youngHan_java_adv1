a326583db4c8c68c2e84b80ce0793955bc12d6a7
AidenRoh <colohrby@gmail.com>
2024-11-27 23:27:01 +0900

    feature: add synchronized collection

---
+++ java-adv1/src/thread/executor/ExecutorUtils.java
@@ -0,0 +1,20 @@
+package thread.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static util.MyLogger.log;
+
+public abstract class ExecutorUtils {
+
+    public static void printState(ExecutorService service) {
+        if (service instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+            int queuedTasks = poolExecutor.getQueue().size();
+            long completedTasks = poolExecutor.getCompletedTaskCount();
+            log("[pool=" + pool + ", active=" + active + ", queuedTasks="
+                    + queuedTasks + ", completedTasks=" + completedTasks + "]");
+        }
+    }
+}
